7047d81bd70a60e054563c08dae74b79c4b0c40f
Gracielibr <gracielibr@hotmail.com>
2025-05-04 18:06:13 -0300

    Delete Banco directory

--- Banco/src/contaCorrente.java
+++
@@ -1,9 +0,0 @@
-public class contaCorrente extends Conta {
-
-    public contaCorrente(Cliente cliente, String numero, String agencia) {
-        super(cliente, numero, agencia);
-    }
-    public void menuOperacoes() {
-            Transacoes.menuOperacoes(this);
-    }
-}
--- Banco/src/contaPoupanca.java
+++
@@ -1,9 +0,0 @@
-public class contaPoupanca extends Conta {
-
-    public contaPoupanca(Cliente cliente, String numero, String agencia) {
-        super(cliente, numero, agencia);
-    }
-    public void menuOperacoes() {
-        Transacoes.menuOperacoes(this);
-    }
-}
